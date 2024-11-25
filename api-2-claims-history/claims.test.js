4e395a76c1b416b77c85749580ef16a515aca439
abL-A2 <amiela@missionreadyhq.com>
2024-11-25 17:25:59 +1300

    refactor: additional comments, removed 7th test case

--- api-2-claims-history/claims.test.js
+++ api-2-claims-history/claims.test.js
@@ -1,17 +1,34 @@
 const request = require("supertest");
 const app = require("./claims");
 
+/*
+Requirements to test for: 
+
+> Input must be a JSON obj containing 'text:' key with a string value.
+
+1. Validation step: invalidate if
+	- data missing (no 'text:' key)
+	- empty
+	- too large strings
+	- non-string inputs
+	- special characters and/or numbers.
+
+1.a. Return error indicating why input is unacceptable; 'input required' or '... must not be empty', 'input exceeds maximum length'.
+
+2. Clean up valid input for processing: trim -> lowercase -> split.
+
+3. Keyword identification: find only predefined keywords: "collide", "crash", "scratch", "bump", and "smash". Include common variants with minor differences (tense or plural forms), i.e. "collided", "crashed", "bumps", "smashes"--though for simplicity we won't include all possible variants. Ignore all other words.
+
+4. Counting keywords: return 'rating' value of 1-5 based on how many keywords were in supplied input, including repeats, i.e, 'I crashed, but the crash wasn't my fault' should return a rating of 2.
+
+5. Evaluate unacceptable 'ratings': invalidate input if number of keywords falls outside 1-5 boundary.
+
+4.b. Return error distinct from the first validation step to make clear error is related to the number of keywords found--none or too many.
+*/
+
 // ! these are test cases for possible input payloads
 
 describe("claims keyword detection API", () => {
-  // test in case empty or accidentally sent data
-  it("should return an error for null inputs", async () => {
-    const response = await request(app).post("/endpoint-goes-here⚠️").send({ text: null });
-
-    expect(response.status).toBe(400);
-    expect(response.body).toHaveProperty("error", "Invalid input text."); // the text field should be 'required' and this shouldn't be a normal return
-  });
-
   // ideal input: keywords are "collide", "crash", "scratch", "bump", and "smash", assuming variants such as tense or plural versions are counted
   it("should return a risk rating for 1-5 keywords", async () => {
     const response = await request(app).post("/endpoint-goes-here⚠️").send({
@@ -25,11 +42,11 @@ describe("claims keyword detection API", () => {
   // valid but not ideal: non-keywords that declare claim history, or misspelled keyword
   it("should return a risk rating for 1-5 keywords that match exactly, excluding similar but misspelled words", async () => {
     const response = await request(app).post("/endpoint-goes-here⚠️").send({
-      text: "I drove my ute into the median, smnashed into a trailer, and scratched against the safety barreir.",
+      text: "I drove my ute into the median, smashed into a trailer, and scrached against the safety barreir",
     });
 
     expect(response.status).toBe(200);
-    expect(response.body.rating).toBe(1); // "drove" & "smnashed" are invalid
+    expect(response.body.rating).toBe(1); // "drove" & "scrached" are invalid
   });
 
   // invalid: sent correct data type & acceptable input, but no keywords present
@@ -63,18 +80,10 @@ describe("claims keyword detection API", () => {
   // * note that the below tests will be rendered superfluous if a regex is used as part of the input validation step, but we'll keep these in and not use regex to focus on the remit of the mission--demonstrate TDD
 
   // invalid: no numbers or special characters
-  it("should return 'Input must be text only.' if there are numbers or special characters", async () => {
-    const response = await request(app).post("/endpoint-goes-here⚠️").send({ text: "Been in 1 crash back in March/" }); // could be split into two tests, one for a number and another for special characters
-
-    expect(response.status).toBe(400);
-    expect(response.body.message).toBe("Input must be text only.");
-  });
-
-  // invalid: no numbers or special characters within otherwise valid keywords
-  it("should return 'Input must be text only.' if there are numbers or special characters", async () => {
+  it("should return 'Input must be text only.' if there are numbers in input", async () => {
     const response = await request(app)
       .post("/endpoint-goes-here⚠️")
-      .send({ text: "Had a c0llide event last week, done scratch,ed up my car." });
+      .send({ text: "Been in 1 crash back in 4 March 2024." }); // could be split into two tests, one for a number and another for special characters
 
     expect(response.status).toBe(400);
     expect(response.body.message).toBe("Input must be text only.");
