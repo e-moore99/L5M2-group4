ebafa5b617474bcc56db25883d14dccbf75af8c5
Evan Moore <evanm@missionreadyhq.com>
2024-11-26 12:13:05 +1300

    passed first unit test

--- api1/api1.js
+++ api1/api1.js
@@ -5,35 +5,29 @@ const test1 = {
 };
 
 // takes the model and splits it into an array of letters
-let model = test1.model.toLowerCase().split('');
+
 
 let result = [];
-
-alphabet.map((letter, i) => {
-    model.map((word, l) => {
+let output = 0;
+const modelToNum = (model) => {
+  console.log(model)
+  let carModel = model.toLowerCase().split('');
+  alphabet.map((letter, i) => {
+    carModel.map((word, l) => {
       if (word === letter) {
-        result.push((i = i + 1));
+        result.push((i + 1));
       }
     });
-  });
-
-  let output = 0;
+  }); 
 for (let i = 0; i < result.length; i++) {
-    output += result[i]
+  output += result[i]
+}
+  return output
 }
 
-const modelToNum = () => {
-    const value = alphabet.map(letter=>{
-        const isIncl = model.includes(letter);
-        return isIncl = letter[i+1]
-    })
-    console.log(value)
-    console.log(isIncl)
-}
-
-console.log(model)
+// console.log(model)
 console.log(test1)
 console.log(result)
 console.log(output)
 
-// module.exports = 
+module.exports = { modelToNum }
--- api1/api1.test.js
+++ api1/api1.test.js
@@ -1,7 +1,10 @@
 // setting up unit tests for API 1 - convert 'model' and 'year' into car value
+const { modelToNum } = require("./api1")
 
 describe("API 1 test cases", ()=> {
-    test.todo("1. Ideal test: enter 'Starlet', 1998 and return 11498");
+    test("1. Ideal test: enter 'Starlet', 1998 and return 11498", () => {
+        expect(modelToNum("Starlet")).toBe(95);
+    });
     test.todo("2. Test model input hybrid char: enter 'ix35', 2013 and return 5313");
     test.todo("3. Test model input num: enter '404', 1969. Return 1969");
     test.todo("4. Test invalid model char: enter 'Santa-Fe', 2012 return error");
