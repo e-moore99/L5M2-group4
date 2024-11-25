bb781a3032dc6f2e5171f6d2db0d3a31d4f458f2
abL-A2 <amiela@missionreadyhq.com>
2024-11-25 16:56:01 +1300

    refactor: added one more test case, note about regex vs TDD

--- api-2-claims-history/claims.test.js
+++ api-2-claims-history/claims.test.js
@@ -1,6 +1,8 @@
 const request = require("supertest");
 const app = require("./claims");
 
+// ! these are test cases for possible input payloads
+
 describe("claims keyword detection API", () => {
   // test in case empty or accidentally sent data
   it("should return an error for null inputs", async () => {
@@ -12,11 +14,9 @@ describe("claims keyword detection API", () => {
 
   // ideal input: keywords are "collide", "crash", "scratch", "bump", and "smash", assuming variants such as tense or plural versions are counted
   it("should return a risk rating for 1-5 keywords", async () => {
-    const response = await request(app)
-      .post("/endpoint-goes-here⚠️")
-      .send({
-        text: "My only claim was a crash into my house's garage door that left a scratch on my car. There are no other crashes.",
-      });
+    const response = await request(app).post("/endpoint-goes-here⚠️").send({
+      text: "My only claim was a crash into my house's garage door that left a scratch on my car. There are no other crashes.",
+    });
 
     expect(response.status).toBe(200);
     expect(response.body.rating).toBe(3); // "crash" "scratch" & "crashes" are all valid
@@ -44,11 +44,9 @@ describe("claims keyword detection API", () => {
 
   // invalid: detected keywords > 5
   it("should return, '6 or more claims keywords found; decline cover' if there are 6+ keywords", async () => {
-    const response = await request(app)
-      .post("/endpoint-goes-here⚠️")
-      .send({
-        text: "There was a crash, saw two cars collide, not just bump but smash, also my cat scratched me. But yeah, boom! Crash, smash! Wasn't me though.",
-      });
+    const response = await request(app).post("/endpoint-goes-here⚠️").send({
+      text: "There was a crash, saw two cars collide, not just bump but smash, also my cat scratched me. But yeah, boom! Crash, smash! Wasn't me though.",
+    });
 
     expect(response.status).toBe(400);
     expect(response.body.message).toBe("6 or more claims keywords found; decline cover.");
@@ -61,9 +59,22 @@ describe("claims keyword detection API", () => {
     expect(response.status).toBe(400);
     expect(response.body.message).toBe("No claims keywords found.");
   });
+
+  // * note that the below tests will be rendered superfluous if a regex is used as part of the input validation step, but we'll keep these in and not use regex to focus on the remit of the mission--demonstrate TDD
+
   // invalid: no numbers or special characters
   it("should return 'Input must be text only.' if there are numbers or special characters", async () => {
-    const response = await request(app).post("/endpoint-goes-here⚠️").send({ text: "Been in 1 crash back in March/" }); // could be a good idea to split this into two tests, one for a number and another for special characters
+    const response = await request(app).post("/endpoint-goes-here⚠️").send({ text: "Been in 1 crash back in March/" }); // could be split into two tests, one for a number and another for special characters
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("Input must be text only.");
+  });
+
+  // invalid: no numbers or special characters within otherwise valid keywords
+  it("should return 'Input must be text only.' if there are numbers or special characters", async () => {
+    const response = await request(app)
+      .post("/endpoint-goes-here⚠️")
+      .send({ text: "Had a c0llide event last week, done scratch,ed up my car." });
 
     expect(response.status).toBe(400);
     expect(response.body.message).toBe("Input must be text only.");
