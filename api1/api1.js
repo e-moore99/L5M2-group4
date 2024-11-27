f4f0055f1c74a74c1e6099321959763c791abb22
Evan Moore <evanm@missionreadyhq.com>
2024-11-27 20:25:52 +1300

    got the endpoint working, now I am working on getting the regex for the year to work

--- api1/api1.js
+++ api1/api1.js
@@ -1,39 +1,78 @@
-const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-let regExModel = /[^a-zA-Z0-9\s]/
-// const test1 = {
-//     "model" : "Starlet",
-//     "year" : 1998
-// };
+const express = require("express");
+const cors = require("cors");
 
+const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+const regExModel = /[^a-zA-Z0-9\s]/;
+let regExYear = /^[1-2][0-9][0-9][0-9]/;
 let result = [];
 let output = 0;
 let insuranceCost = 0;
-const modelToNum = (model, year) => {
-  console.log(`model: `+model)
-  if (model.search(regExModel) != -1) {
-    console.log("error") 
-    return "error"
-  } else {
-  let carModel = model.toLowerCase().split('');
-  alphabet.map((letter, i) => {
-    carModel.map((word, l) => {
-      if (word === letter) {
-        result.push((i + 1));
-      }
-    });
-  }); 
-for (let i = 0; i < result.length; i++) {
-  output += result[i]
+
+const app = express();
+app.use(express.json())
+app.use(cors());
+
+app.post("/api1", (req,res) => {
+  let model = req.body.model;
+  let year = JSON.parse(req.body.year);
+  // let carModel = model.toLowerCase().split('');
+  // insert function here
+    console.log(`model: `+model)
+    if (model.search(regExModel) != -1) {
+      console.log("error") 
+      return "error"
+    } else {
+    let carModel = model.toLowerCase().split('');
+    alphabet.map((letter, i) => {
+      carModel.map((word, l) => {
+        if (word === letter) {
+          result.push((i + 1));
+        }
+      });
+    }); 
+  for (let i = 0; i < result.length; i++) {
+    output += result[i]
+  }
+  console.log(output)
+  insuranceCost = (output *100) + year;
+  console.log(`insurance cost`+insuranceCost)
 }
-insuranceCost = output *100 + year;
-console.log(`insurance cost`+insuranceCost)
-  return insuranceCost
-    
-}
-}
+res.send(`The premium estimate for your ${year} ${model} is $${insuranceCost}.`)
+const body = req.body;
+console.log(body)
+res.json(body)
+})
+
+// const modelToNum = (model, year) => {
+//   console.log(`model: `+model)
+//   if (model.search(regExModel) != -1) {
+//     console.log("error") 
+//     return "error"
+//   } else {
+//   let carModel = model.toLowerCase().split('');
+//   alphabet.map((letter, i) => {
+//     carModel.map((word, l) => {
+//       if (word === letter) {
+//         result.push((i + 1));
+//       }
+//     });
+//   }); 
+// for (let i = 0; i < result.length; i++) {
+//   output += result[i]
+// }
+// insuranceCost = output *100 + year;
+// console.log(`insurance cost`+insuranceCost)
+//   return insuranceCost
+// }
+// }
 
 // console.log(model)
 // console.log(test1)
 console.log(`insurance cost`+ insuranceCost)
 
-module.exports = { modelToNum }
+// module.exports = { modelToNum }
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`listening on http://localhost:${PORT}`)
+})
--- api1/api1.test.js
+++ api1/api1.test.js
@@ -2,18 +2,18 @@
 const { modelToNum } = require("./api1")
 
 describe("API 1 test cases", ()=> {
-    // test("1. Ideal test: enter 'Starlet', 1998 and return 11498", () => {
-    //     expect(modelToNum("Starlet", 1998)).toBe(11498);
-    // });
+    test("1. Ideal test: enter 'Starlet', 1998 and return 11498", () => {
+        expect(modelToNum("Starlet", 1998)).toBe(11498);
+    });
     // test("2. Test model input hybrid char: enter 'ix35', 2013 and return 5313", () => {
     //     expect(modelToNum("ix35", 2013)).toBe(5313);
     // });
     // test("3. Test model input num: enter '404', 1969. Return 1969", () => {
     //     expect(modelToNum("404", 1969)).toBe(1969);
     // });
-    test("4. Test invalid model char: enter 'Santa-Fe', 2012 return error", () => {
-        expect(modelToNum("Santa-Fe", 2012)).toBe("error")
-    });
+    // test("4. Test invalid model char: enter 'Santa-Fe', 2012 return error", () => {
+    //     expect(modelToNum("Santa-Fe", 2012)).toBe("error")
+    // });
     test.todo("5. Test negative year: enter 'Vitz', -2000 return error");
     test.todo("6. Test invalid year input type: enter 'Demio', twenty thirteen return error");
 })
